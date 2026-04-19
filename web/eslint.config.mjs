4181618f0036026e8aaaf3baf426e8b7ab211e85
Nerdodrome <willouprod@live.fr>
2026-04-19 11:34:04 +0000

    fix: eslint flat config sans FlatCompat

--- web/eslint.config.mjs
+++ web/eslint.config.mjs
@@ -1,28 +1,18 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({ baseDirectory: __dirname });
+import pluginNext from "@next/eslint-plugin-next";
 
 export default [
-  ...compat.extends("next/core-web-vitals"),
   {
+    files: ["**/*.{ts,tsx,js,jsx}"],
+    plugins: {
+      "@next/next": pluginNext,
+    },
     rules: {
+      ...pluginNext.configs.recommended.rules,
+      ...pluginNext.configs["core-web-vitals"].rules,
       "@next/next/no-img-element": "off",
       "react-hooks/exhaustive-deps": "off",
       "no-unused-vars": "off",
       "@typescript-eslint/no-unused-vars": "off",
-      "unused-imports/no-unused-imports": "warn",
-      "unused-imports/no-unused-vars": ["warn", {
-        vars: "all",
-        varsIgnorePattern: "^_",
-        args: "after-used",
-        argsIgnorePattern: "^_",
-        ignoreRestSiblings: true
-      }]
-    }
-  }
+    },
+  },
 ];
